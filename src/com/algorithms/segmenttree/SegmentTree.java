26e5bf0dfb4f2822d736be99a2a9fe8f05c27007
李硕 <lishuo_striving@163.com>
2021-05-11 11:10:05 +0800

    just a push

---
+++ src/com/algorithms/segmenttree/SegmentTree.java
@@ -0,0 +1,57 @@
+package com.algorithms.segmenttree;
+
+/**
+ * @author itjunjun  2021/5/11 10:18
+ */
+public class SegmentTree<E> {
+
+    private E[] data;
+    private E[] tree;
+
+    public SegmentTree(E[] arr) {
+        data = (E[]) new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+        tree = (E[]) new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length - 1);
+    }
+
+    /**
+     * 在treeIndex的位置创建表示区间[l..r]的线段树
+     * @param treeIndex
+     * @param l
+     * @param r
+     */
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("index is illegal");
+        }
+        return data[index];
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+}
