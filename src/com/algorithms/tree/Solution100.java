57309f88596398248167366c1b53c46316e8f9ec
李硕 <lishuo_striving@163.com>
2021-05-12 09:27:41 +0800

    just a push

---
+++ src/com/algorithms/tree/Solution100.java
@@ -0,0 +1,38 @@
+package com.algorithms.tree;
+
+/**
+ * @author itjunjun  2021/5/11 11:35
+ */
+public class Solution100 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+
+}
