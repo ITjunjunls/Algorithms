82dcad477a1b0c405e451ace5f10b2819bfc6436
李硕 <lishuo_striving@163.com>
2021-05-19 21:28:55 +0800

    just a push

---
+++ src/com/algorithms/tree/Solution235.java
@@ -0,0 +1,37 @@
+package com.algorithms.tree;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author itjunjun  2021/5/19 11:32
+ */
+public class Solution235 {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+}
